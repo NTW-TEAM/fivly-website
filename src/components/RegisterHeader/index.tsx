c6410c940b290bbae881f8099ee008eb1c7469af
Thomas Goillot <thomadgllt@gmail.com>
2024-03-26 17:48:10 +0100

    style(register) : rework ui of register page

---
+++ src/components/RegisterHeader/index.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+import DarkModeSwitcher from "../Header/DarkModeSwitcher";
+
+const Header = (props: {
+  sidebarOpen: string | boolean | undefined;
+  setSidebarOpen: (arg0: boolean) => void;
+}) => {
+  return (
+    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+        <div className="flex items-center gap-2 sm:gap-4">
+          
+          <Link className="block flex-shrink-0" href="/">
+            <Image
+              width={32}
+              height={32}
+              src={"/images/logo/logo-icon.svg"}
+              alt="Logo"
+            />
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-3 2xsm:gap-7">
+          <ul className="flex items-center gap-2 2xsm:gap-4">
+            {/* <!-- Dark Mode Toggler --> */}
+            <DarkModeSwitcher />
+            {/* <!-- Dark Mode Toggler --> */}
+          </ul>
+
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
