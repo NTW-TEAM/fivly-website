bbe0d273dfc69a16844bce4ae1a6f40644cccead
Wissdx <62649911+Wissdx@users.noreply.github.com>
2024-04-18 14:25:59 +0200

    chores(login): fix text color of login password field

--- src/components/Auth/SignInForm.tsx
+++ src/components/Auth/SignInForm.tsx
@@ -43,7 +43,7 @@ const SignInForm = () => {
                         type="password"
                         name="password"
                         placeholder="6+ Characters, 1 Capital letter"
-                        className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-white outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         required
                     />
 
