9b96bd7b5466f2394fdefce9984938c4fa034e08
Wissdx <62649911+Wissdx@users.noreply.github.com>
2024-04-18 14:18:52 +0200

    feat(login): LogoutButton

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
-import { toast } from "react-toastify";
-import { RxExit } from "react-icons/rx";
 import { FaUserCircle } from "react-icons/fa";
+import LogoutButton from "./LogoutButton";
 
 const DropdownUser = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -62,9 +60,8 @@ const DropdownUser = () => {
         ref={dropdown}
         onFocus={() => setDropdownOpen(true)}
         onBlur={() => setDropdownOpen(false)}
-        className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark ${
-          dropdownOpen === true ? "block" : "hidden"
-        }`}
+        className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark ${dropdownOpen === true ? "block" : "hidden"
+          }`}
       >
         <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-4.5 dark:border-strokedark">
           <li>
@@ -74,11 +71,7 @@ const DropdownUser = () => {
             </Link>
           </li>
         </ul>
-        <Link href="/auth/logout" onClick={() => { toast.success("Déconnexion réussie") }}
-          className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
-          <RxExit />
-          Déconnexion
-        </Link>
+        <LogoutButton />
       </div>
       {/* <!-- Dropdown End --> */}
     </div>
