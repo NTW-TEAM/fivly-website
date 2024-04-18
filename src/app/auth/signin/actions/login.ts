0d014d331bf1eb943b5981573a4966a05b5e21b6
Wissdx <62649911+Wissdx@users.noreply.github.com>
2024-04-18 14:18:08 +0200

    chores(login): remove useless imports

--- src/app/auth/signin/actions/login.ts
+++ src/app/auth/signin/actions/login.ts
@@ -7,46 +7,45 @@ import { redirect } from "next/navigation";
 
 export async function login(formData: FormData) {
 
-    const email = formData.get("email");
-    const password = formData.get("password");
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    if (!email || !password) {
-      return {
-        status : "error",
-        message : "Veuillez remplir tous les champs."
-      };
-    }
-    
-    const response = await fetch(`${process.env.API_URL}/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ "email": email, "password": password }),
+  if (!email || !password) {
+    return {
+      status: "error",
+      message: "Veuillez remplir tous les champs."
+    };
+  }
+
+  const response = await fetch(`${process.env.API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ "email": email, "password": password }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    const token = data.access_token;
+
+    const user = jwt.verify(token, process.env.JWT_SECRET!);
+
+    cookies().set("auth_token", token, {
+      secure: process.env.NODE_ENV === "production",
     });
 
-    if (response.ok) {
-        const data = await response.json();
-        const token = data.access_token;
-        
-        const user = jwt.verify(token, process.env.JWT_SECRET!);
-        
-        cookies().set("auth_token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-        });
+    revalidatePath("/");
+    redirect("/");
 
-        revalidatePath("/");
-        redirect("/");
-        
-    } else {
-        const data = await response.json();
-        // data.message is an array of error messages
-        
-        return {
-          status : "error",
-          message : data.message
-        };
-    }
+  } else {
+    const data = await response.json();
+    // data.message is an array of error messages
+
+    return {
+      status: "error",
+      message: data.message
+    };
+  }
 
 }   
