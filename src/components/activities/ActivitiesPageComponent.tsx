11a49dc2e6bf90643aa8fada394f7d4a5f6dc265
Thomas <222teegee@gmail.com>
2024-06-02 16:32:31 +0200

    feat(activities) : wip impl of activities admin panel

---
+++ src/components/activities/ActivitiesPageComponent.tsx
@@ -0,0 +1,86 @@
+"use client";
+import Breadcrumb from "../Breadcrumbs/Breadcrumb";
+import DefaultLayout from "../Layouts/DefaultLayout";
+import CardDataStats from "../CardDataStats";
+import React from "react";
+import { MdOutlineTypeSpecimen } from "react-icons/md";
+import { ActivityType } from "@/types/activityType";
+import localApi from "@/services/localAxiosApi";
+import { Activity } from "@/types/activity";
+import { FaRegCalendarAlt, FaUsers } from "react-icons/fa";
+import TableActivities from "./TableActivities";
+
+const ActivitiesPageComponent = () => {
+    const getAllActivityType = async () => {
+        return new Promise<ActivityType[]>((resolve, reject) => {
+        localApi
+            .get(`/api/activity-types`)
+            .then((response) => {
+                if (response.status === 200) {
+                    resolve(response.data);
+                }
+            })
+            .catch((error) => {
+                console.error("error", error);
+                reject([]);
+            });
+        });
+    };
+
+    const getAllActivities = async () => {
+        return new Promise<Activity[]>((resolve, reject) => {
+        localApi
+            .get(`/api/activities`)
+            .then((response) => {
+                if (response.status === 200) {
+                    resolve(response.data);
+                }
+            })
+            .catch((error) => {
+                console.error("error", error);
+                reject([]);
+            });
+        });
+    };
+
+    const [activityTypes, setActivityTypes] = React.useState<ActivityType[]>([]);
+    const [activities, setActivities] = React.useState<Activity[]>([]);
+
+    React.useEffect(() => {
+        const fetchActivityTypes = async () => {
+            const activityTypes = await getAllActivityType();
+            setActivityTypes(activityTypes);
+        };
+
+        const fetchActivities = async () => {
+            const activities = await getAllActivities();
+            setActivities(activities);
+        };
+
+        fetchActivityTypes();
+        fetchActivities();
+    }, []);
+
+    return (
+        <DefaultLayout>
+        <Breadcrumb pageName="Permissions"/>
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
+            <CardDataStats title="Activités au total" total={activities.length.toString()} rate="">
+                <FaRegCalendarAlt className="fill-primary dark:fill-white"/>
+            </CardDataStats>
+            <CardDataStats title="Type d'activité" total={activityTypes.length.toString()} rate="">
+                <MdOutlineTypeSpecimen className="fill-primary dark:fill-white"/>
+            </CardDataStats>
+        </div>
+
+        <div className="mt-4 flex flex-col gap-10">
+            <TableActivities activities={activities} setActivities={setActivities} />
+        </div>
+        </DefaultLayout>
+    );
+    };
+
+
+
+export default ActivitiesPageComponent;
