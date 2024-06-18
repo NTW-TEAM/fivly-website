db4fd6f3faf637f1c9351cd93cb2977492a00730
Thomas <222teegee@gmail.com>
2024-06-19 00:19:25 +0200

    fix(profile) : remove profile edit

--- src/components/Profile/ProfilePageComponent.tsx
+++ src/components/Profile/ProfilePageComponent.tsx
@@ -62,7 +62,7 @@ const ProfilePageComponent = () => {
                 height: "auto",
               }}
             />
-            <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4">
+{/*             <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4">
               <label
                 htmlFor="cover"
                 className="flex cursor-pointer items-center justify-center gap-2 rounded bg-primary px-2 py-1 text-sm font-medium text-white hover:bg-opacity-80 xsm:px-4"
@@ -78,7 +78,7 @@ const ProfilePageComponent = () => {
                 </span>
                 <span>Edit</span>
               </label>
-            </div>
+            </div> */}
           </div>
           <div className="px-4 pb-6 text-center lg:pb-8 xl:pb-11.5">
             <div className="relative z-30 mx-auto -mt-22 h-30 w-full max-w-30 rounded-full bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
