dbdb9c08072c3855e389554debe132549a51cd5a
Thomas Goillot <thomadgllt@gmail.com>
2024-07-12 14:14:47 +0200

    feat(finance) : impl dashboard for crowdfunding and donation

--- src/app/finances/page.tsx
+++ src/app/finances/page.tsx
@@ -8,6 +8,7 @@ import { FaHandsHelping } from "react-icons/fa";
 import { AiFillFund } from "react-icons/ai";
 import { GiReceiveMoney } from "react-icons/gi";
 import DashboardChart from "@/components/Finances/DashboardChart";
+import FinancePageComponent from "@/components/Finances/FinancePageComponent";
 
 export const metadata: Metadata = {
   title: TITLE + " - Dashboard Finances",
@@ -15,41 +16,7 @@ export const metadata: Metadata = {
 };
 
 const MembresPage = () => {
-  return (
-    <DefaultLayout>
-      <Breadcrumb pageName="Dashboard Finances" />
-
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
-        <CardDataStats
-          title="Total des fonds récoltés"
-          total="200,000 €"
-          rate="+ 10%"
-          levelUp
-        >
-          <AiFillFund className="fill-primary dark:fill-white" />
-        </CardDataStats>
-        <CardDataStats
-          title="Nombre de dons"
-          total="100"
-          rate="- 25 %"
-          levelDown
-        >
-          <GiReceiveMoney className="fill-primary dark:fill-white" />
-        </CardDataStats>
-        <CardDataStats
-          title="Cotisations en attente"
-          total="50"
-          rate="+ 50 %"
-          levelUp
-        >
-          <FaHandsHelping className="fill-primary dark:fill-white" />
-        </CardDataStats>
-      </div>
-
-      <DashboardChart />      
-
-    </DefaultLayout>
-  );
+  return <FinancePageComponent />;
 };
 
 export default MembresPage;
--- src/components/Finances/ChartRepartition.tsx
+++ src/components/Finances/ChartRepartition.tsx
@@ -1,73 +1,67 @@
+
 import { ApexOptions } from "apexcharts";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
 
-interface ChartThreeState {
+interface ChartRepartitionProps {
+  title: string;
+  labels: string[];
   series: number[];
+  colors: string[];
 }
 
-const options: ApexOptions = {
-  chart: {
-    fontFamily: "Satoshi, sans-serif",
-    type: "donut",
-  },
-  colors: ["#3C50E0", "#6577F3", "#8FD0EF"],
-  labels: ["Dons", "Cotisations", "Crowdfunding"],
-  legend: {
-    show: false,
-    position: "bottom",
-  },
-
-  plotOptions: {
-    pie: {
-      donut: {
-        size: "65%",
-        background: "transparent",
-      },
+const ChartRepartition: React.FC<ChartRepartitionProps> = ({ title, labels, series, colors }) => {
+  const options: ApexOptions = {
+    chart: {
+      fontFamily: "Satoshi, sans-serif",
+      type: "donut",
     },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  responsive: [
-    {
-      breakpoint: 2600,
-      options: {
-        chart: {
-          width: 380,
+    colors: colors,
+    labels: labels,
+    legend: {
+      show: false,
+      position: "bottom",
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          size: "65%",
+          background: "transparent",
         },
       },
     },
-    {
-      breakpoint: 640,
-      options: {
-        chart: {
-          width: 200,
+    dataLabels: {
+      enabled: false,
+    },
+    responsive: [
+      {
+        breakpoint: 2600,
+        options: {
+          chart: {
+            width: 380,
+          },
         },
       },
-    },
-  ],
-};
-
-const ChartRepartition: React.FC = () => {
-  const [state, setState] = useState<ChartThreeState>({
-    series: [65, 34, 12],
-  });
-
-  const handleReset = () => {
-    setState((prevState) => ({
-      ...prevState,
-      series: [65, 34, 12],
-    }));
+      {
+        breakpoint: 640,
+        options: {
+          chart: {
+            width: 200,
+          },
+        },
+      },
+    ],
   };
-  handleReset;
+
+  const total = series.reduce((acc, value) => acc + value, 0);
+  const percentages = series.map((value) => (value / total) * 100);
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-5">
+    <div className="rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="mb-3 justify-between gap-4 sm:flex">
         <div>
           <h5 className="text-xl font-semibold text-black dark:text-white">
-            Répartition des dons, cotisations et crowdfunding
+            {title}
           </h5>
         </div>
         <div>
@@ -112,40 +106,24 @@ const ChartRepartition: React.FC = () => {
         <div id="chartThree" className="mx-auto flex justify-center">
           <ReactApexChart
             options={options}
-            series={state.series}
+            series={series}
             type="donut"
           />
         </div>
       </div>
 
       <div className="-mx-8 flex flex-wrap items-center justify-center gap-y-3">
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#3C50E0]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Dons </span>
-              <span> 54% </span>
-            </p>
+        {labels.map((label, index) => (
+          <div key={index} className="w-full px-8 sm:w-1/2">
+            <div className="flex w-full items-center">
+              <span className={`mr-2 block h-3 w-full max-w-3 rounded-full`} style={{ backgroundColor: colors[index] }}></span>
+              <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
+                <span> {label} </span>
+                <span> {percentages[index].toFixed(2)}% </span>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#6577F3]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Cotisations </span>
-              <span> 34% </span>
-            </p>
-          </div>
-        </div>
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#8FD0EF]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Crowdfunding </span>
-              <span> 12% </span>
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
