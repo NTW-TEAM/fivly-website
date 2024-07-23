99ec4ea712d4f7e56445b5c176db7ba012d05d45
Thomas <222teegee@gmail.com>
2024-07-24 01:24:56 +0200

    fix(roles) : update permissions display

--- src/components/members/DisplayScopesMembers.tsx
+++ src/components/members/DisplayScopesMembers.tsx
@@ -1,26 +1,24 @@
-import {Scopes} from "@/types/Scopes";
-import {useState} from "react";
+import React from "react";
+import { Scopes } from "@/types/Scopes";
 
-const ScopesMembersDisplay = ({ scopesData }: { scopesData: Scopes[] }) => {
-  const [scopes, setScopes] = useState(scopesData);
-
-  if(scopes.length <= 0) return (<div>Aucune Permission</div>);
+const DisplayScopesMembers = ({ scopesData }: { scopesData: Scopes[] }) => {
+  if (scopesData.length <= 0) return <div>Aucune Permission</div>;
 
   return (
-    <div className="flex items-center gap-1">
-      {scopes.map((scope: Scopes, i) =>
-        scope.name.toLowerCase() !== "member" ? (
-          <div key={scope.name} className="flex items-center gap-1">
-            <div className="flex items-center gap-2 rounded bg-purple-700 px-2 py-1 text-sm text-white">
-              <span>{scope.description}</span>
-            </div>
-          </div>
-        ) : (
-          <div key={scope.name}></div>
-        ),
-      )}
-    </div>
+      <div className="flex items-center gap-1">
+        {scopesData.map((scope: Scopes) =>
+            scope.name.toLowerCase() !== "member" ? (
+                <div key={scope.name} className="flex items-center gap-1">
+                  <div className="flex items-center gap-2 rounded bg-purple-700 px-2 py-1 text-sm text-white">
+                    <span>{scope.description}</span>
+                  </div>
+                </div>
+            ) : (
+                <div key={scope.name}></div>
+            )
+        )}
+      </div>
   );
 };
 
-export default ScopesMembersDisplay;
+export default DisplayScopesMembers;
